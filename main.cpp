5ef2a4686361142304d81f527fb6c1e3e70a4df8
ACarfi <carfi.alessandro@gmail.com>
2021-11-09 15:39:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
+
 int main() {
-   return 0;
+    bool flag = true;
+    int n=0;
+    int fibonacci_p= 1, fibonacci=1;
+
+    cin >> n;
+
+    while(fibonacci <= n){
+        if(flag){
+            flag = false;
+            cout << fibonacci_p <<endl;
+        }else {
+            cout << fibonacci << endl;
+            int temp = fibonacci;
+            fibonacci += fibonacci_p;
+            fibonacci_p = temp;
+        }
+    }
+    return 0;
 }
