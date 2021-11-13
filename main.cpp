557ceb847275d92ffbff2a7d0c5a1cec852371ab
Edoardo Toscano <91884355+EdoTosco@users.noreply.github.com>
2021-11-13 20:48:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
 using namespace std;
 
-
 int main() {
-    bool flag = true;
-    int n=0;
-    int fibonacci_p= 1, fibonacci=1;
 
-    cin >> n;
+    int fibonacci=0, Primo_Valore=1, Secondo_Valore=1, RFibo=1, massimo=0;
 
-    while(fibonacci <= n){
-        if(flag){
-            flag = false;
-            cout << fibonacci_p <<endl;
-        }else {
-            cout << fibonacci << endl;
-            int temp = fibonacci;
-            fibonacci += fibonacci_p;
-            fibonacci_p = temp;
+    cin >> massimo;
+
+    if (massimo >= 2){
+
+        cout << Primo_Valore << endl;
+        cout << Secondo_Valore << endl;
+
+        for(int i=2; i <= massimo; i++){
+
+            RFibo = Primo_Valore + Secondo_Valore;
+
+            Primo_Valore=Secondo_Valore;
+            Secondo_Valore=RFibo;
+
+            if(RFibo<=massimo){
+                cout<<RFibo<<endl;
+            }else{
+                break;
+            }
         }
+    }else{
+        cout<<"errore"<<" ";
     }
+
     return 0;
-}
+    }
